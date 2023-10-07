5e00f93453f0a4528524fc0691500db753c0805b
Zheka <MegalomaniacJester@gmail.com>
2023-10-07 19:49:12 +0300

    2

--- Models/KinoDb0410Context.cs
+++ Models/KinoDb0410Context.cs
@@ -29,6 +29,11 @@ public partial class KinoDb0410Context : DbContext
 
     public virtual DbSet<Vote> Votes { get; set; }
 
+
+
+
+
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=DESKTOP-T5P3GVP;Database=KinoDb0410;Trusted_Connection=True;TrustServerCertificate=True;");
